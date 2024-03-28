fe5ddca8bd8d495fc71af6c0e8a9d0ea978c44ac
jake-brennan714 <jake.brennan714@gmail.com>
2024-03-28 13:20:23 -0400

    AbstractWeekdayTest has static vars

--- src/test/java/com/zipcodewilmington/froilansfarm/weekdayTests/AbstractWeekdayTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/weekdayTests/AbstractWeekdayTest.java
@@ -2,14 +2,23 @@ package com.zipcodewilmington.froilansfarm.weekdayTests;
 
 import com.zipcodewilmington.froilansfarm.SharedFixture;
 import org.junit.BeforeClass;
-import org.junit.runners.Suite.SuiteClasses;
-import org.junit.runners.Suite;
-import org.junit.runner.RunWith;
+import org.junit.Test;
+//import org.junit.runners.Suite.SuiteClasses;
+//import org.junit.runners.Suite;
+//import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
 
 //@RunWith(Suite.class)
 //@SuiteClasses({SundayTest.class, MondayTest.class, TuesdayTest.class, WednesdayTest.class, ThursdayTest.class, FridayTest.class, SaturdayTest.class})
 public class AbstractWeekdayTest {
     static SharedFixture fixture;
+    static Stable[] stables;
+    static ChickenCoop[] coops;
+    static Farmer froilan;
+    static Pilot froilanda;
+    static Farm froilansFarm;
+
 
     // TODO : Make these testable!!!
     @BeforeClass
@@ -17,26 +26,44 @@ public class AbstractWeekdayTest {
         fixture = new SharedFixture();
         fixture.initialize();
 
-        for (Stable s : fixture.getStables()) {
-            for (Horse h : s) {
-                fixture.getFroilan().mount(h);
-                fixture.getFroilan().dismount(h);
-                h.eat(new EarCorn());
-                h.eat(new EarCorn());
-                h.eat(new EarCorn());
-            }
-        }
+        stables = fixture.getStables();
 
-        fixture.getFroilan().eat(new EarCorn());
-        fixture.getFroilanda().eat(new Tomato());
+        coops = fixture.getCoops();
 
-        for (int i = 0; i < 2; i++) {
-            fixture.getFroilan().eat(new Tomato());
-            fixture.getFroilanda().eat(new EarCorn());
-            fixture.getFroilanda().eat(new EdibleEgg());
-        }
-        for (int i = 0; i < 5; i++) {
-            fixture.getFroilan().eat(new EdibleEgg());
-        }
+        froilan = fixture.getFroilan();
+
+        froilanda = fixture.getFroilanda();
+
+        froilansFarm = fixture.getFroilansFarm();
+    }
+
+    @Test
+    public void froilanInstanceTestFarmer() {
+        assertTrue(froilan instanceof Farmer);
+    }
+
+    @Test
+    public void froilanInheritanceTestPerson() {
+        assertTrue(froilan instanceof Person);
+    }
+
+    @Test
+    public void froilanInheritanceTestBotanist() {
+        assertTrue(froilan instanceof Botanist);
+    }
+
+    @Test
+    public void froilanInheritanceTestRider() {
+        assertTrue(froilan instanceof Rider);
+    }
+
+    @Test
+    public void froilanInheritanceTestEater() {
+        assertTrue(froilan instanceof Eater);
+    }
+
+    @Test
+    public void froilanInheritanceTestNoiseMaker() {
+        assertTrue(froilan instanceof NoiseMaker);
     }
 }
